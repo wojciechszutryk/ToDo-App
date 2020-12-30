cfc26fb80046134a8812ab56057abcf7003c13dd
wojciechszutryk <wojtekszutryk@gmail.com>
2020-12-30 14:36:03 +0100

    add search input handler

--- toDoApp/Application.js
+++ toDoApp/Application.js
@@ -1,15 +1,25 @@
 class Application{
     constructor() {
+        let that = this;
+        this.currentLiList = []
         this.ul = document.querySelector('.wrapper ul');
-        this.li = [...document.querySelectorAll('.wrapper ul li')];
         this.tasksHistory = new TasksHistory();
         this.tasksNumber = document.querySelector('h2 span');
         this.inputAdd = document.querySelector('.wrapper input');
         this.addBtn = document.querySelector('.wrapper button');
-        this.addBtn.addEventListener('click', this.newTaskRender.bind(this,event))
+        this.inputSearch = document.querySelector('.wrapper h2 + label input');
+        this.addBtn.addEventListener('click', this.newTaskRender.bind(this))
+        this.inputSearch.addEventListener('input', this.searchRender.bind(that))
     }
-    newTaskRender(){
+    updateCurrentLiList(){
+        this.currentLiList = [...this.ul.childNodes];
+    }
+    newTaskRender(event){
         event.preventDefault();
+        this.inputSearch.value = '';
+        for(let currentLiItem of this.currentLiList){
+            this.ul.appendChild(currentLiItem);
+        }
         if (this.inputAdd.value) {
             for (let li of [...document.querySelectorAll('.wrapper ul li')]){
                 if( li.textContent === this.inputAdd.value){
@@ -21,13 +31,14 @@ class Application{
                 }
             }
             const createdTask = this.tasksHistory.addTask(this.inputAdd.value);
-            this.tasksNumber.textContent++;
+            this.tasksNumber.textContent = this.currentLiList.length+1;
             this.ul.appendChild(createdTask);
             const that = this;
             createdTask.querySelector('button').addEventListener('click', function () {
                 that.removeTask(this);
             })
             this.inputAdd.value = '';
+            this.updateCurrentLiList()
         }
         else alert('task con not be empty')
     }
@@ -39,5 +50,25 @@ class Application{
         for( let i = index+1; i < this.ul.childElementCount+1; i++){
             document.querySelector(`ul li:nth-child(${i})`).id -= 1;
         }
+        this.updateCurrentLiList()
+    }
+    searchRender(e){
+        for(let currentLiItem of this.currentLiList){
+            this.ul.appendChild(currentLiItem);
+        }
+        const phrase = e.target.value;
+        const result = [];
+        let counter = 0;
+        const li = [...document.querySelectorAll('.wrapper ul li')];
+        this.ul.textContent = '';
+        for(let i of li){
+            if(i.textContent.toLowerCase().includes(phrase)) {
+                this.ul.appendChild(i);
+                counter++;
+            }
+        }
+        this.tasksNumber.textContent = counter;
+        console.log(counter);
+
     }
 }
